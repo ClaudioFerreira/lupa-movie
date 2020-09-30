ccb5ef807c37237c0d077a344a53ad49cb4f9f78
Claudio Ferreira <claudio.zero0@gmail.com>
2020-09-30 15:32:08 -0300

    add search bar

--- src/modules/main/index.tsx
+++ src/modules/main/index.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './style.css'
 
 export default function Main() {
+
+    const [search, setSearch] = useState('')
+    const [filme, setFilme] = useState(true)
+
+    function handleSelect() {
+        setFilme(!filme)
+    }
+
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+    }
+
     return (
         <>
-            <h1>Main</h1>
+            <div className="seach-bar">
+                <div className="container">
+                    <form onSubmit={e => handleSubmit(e)}>
+                        <input
+                            type="text"
+                            placeholder="Buscar"
+                            value={search}
+                            onChange={e => setSearch(e.target.value)}
+                        />
+                        <input
+                            type="radio"
+                            name="type"
+                            id="filme"
+                            checked={filme}
+                            onChange={handleSelect}
+                        />
+                        <label htmlFor="filme">Filmes</label>
+                        <input
+                            type="radio"
+                            name="type"
+                            id="serie"
+                            checked={!filme}
+                            onChange={handleSelect}
+                        />
+                        <label htmlFor="serie">Series</label>
+                        <button type="submit">
+                            Buscar
+                    </button>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
